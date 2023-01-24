9805ed009eb813b76b449365472831f74d69e52d
MateoGalan616 <117777169+MateoGalan616@users.noreply.github.com>
2023-01-24 09:51:38 -0500

    cualquier mensaje que describa los cambios realizados.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card } from 'antd';
+import React from "react";
+import { Card } from "antd";
 
 const { Meta } = Card;
 
@@ -7,7 +7,12 @@ const App: React.FC = () => (
   <Card
     hoverable
     style={{ width: 240 }}
-    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+    cover={
+      <img
+        alt="example"
+        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      />
+    }
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
---
+++ src/Avatar.js
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+    <img
+      className={avatar}
+      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      alt="Gregorio Y. Zara"
+    />
+  );
+}
